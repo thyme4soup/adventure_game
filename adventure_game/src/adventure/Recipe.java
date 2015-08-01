585a4c2fa27282c57b58889c80d1f9c6e0ee7f9d
Thomas Qian <tom07qian@gmail.com>
2015-08-01 12:44:02 -0700

    structure for recipe

--- adventure_game/src/adventure/Recipe.java
+++ adventure_game/src/adventure/Recipe.java
@@ -6,33 +6,57 @@ import java.util.Map;
 public class Recipe {
 	private static Map<ItemStack[], ItemStack> recipes; //input, output
 	
+	//stores the defined recipes in map
 	public static void createRecipes() {
 		//init recipes
-		recipes = new HashMap<ItemStack[], ItemStack>();
+		recipes = new HashMap<>();
 		
 		ItemStack stick = new ItemStack(new Item("stick"), 1);
 		ItemStack rock = new ItemStack(new Item("rock"), 1);
 		ItemStack hammer = new ItemStack(new Item("hammer"), 1);
-		recipes.put(
-				new ItemStack[] {
-						stick,
-						rock
-				}, hammer);
 		
+		recipes.put(new ItemStack[] {stick, rock}, hammer);
 	}
 	
-	public static boolean canCraft(Item i, Inventory inv) {
+	/**
+	 * Checks if the input is a valid recipe
+	 * @param in the item stack to check
+	 * @return the Item if the recipe is valid, else null
+	 */
+	public Item isRecipe(ItemStack[] in) {
+		
+		return null;
+	}
+	
+	/**
+	 * Checks if the player has the items to craft an item, private because called in craft
+	 * in order to reduce confusion and clutter elsewhere
+	 * @param i the desired item to craft
+	 * @param inv the players inventory
+	 * @return if the player has items to craft an item
+	 */
+	private boolean canCraft(ItemStack i, Inventory inv) {
+		
+		return false;
+	}
+	
+	/**
+	 * crafts the item into the players inventory if possible
+	 * @param i the item to craft
+	 * @param inv the players inventory
+	 * @return if the craft was successful
+	 */
+	public boolean craft(ItemStack i, Inventory inv) {
+		if(!canCraft(i, inv)) return false;
 		return true;
-	}
-	
-	public void craft(Item i, Inventory inv) {
-		
 	}
 }
 
+//helper class
 class ItemStack {
 	Item item;
 	int quantity;
+	
 	public ItemStack(Item item, int n) {
 		this.item = new Item(item.toString());
 		quantity = n;
