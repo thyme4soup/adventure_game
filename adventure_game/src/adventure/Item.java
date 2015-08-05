8ae48725810d629df8fe2c1cc64e30f8fe7fd880
thomasqian <tom07qian@gmail.com>
2015-08-05 11:45:27 -0700

    Added a database of valid items. Item constructor will check against the database when creating new Item.

--- adventure_game/src/adventure/Item.java
+++ adventure_game/src/adventure/Item.java
@@ -2,8 +2,21 @@ package adventure;
 
 public class Item {
 	public String name;
+	private static String[] itemList = new String[] {
+		"stick",
+		"rock",
+		"hammer",
+		"sharprock"
+	};
 	
 	public Item(String name) {
+		for(int i = 0; i < itemList.length; i++) {
+			if(name.contains(itemList[i])) {
+				name = itemList[i];
+				break;
+			}
+			if(i == itemList.length - 1) name = "unknown";
+		}
 		this.name = name;
 	}
 	
