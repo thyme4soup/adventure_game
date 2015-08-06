ca932e9b7388bd2f858c4a51a1dc3e04ef44de15
thomasqian <tom07qian@gmail.com>
2015-08-06 12:14:12 -0700

    updated sharp rock

--- adventure_game/src/adventure/Interaction.java
+++ adventure_game/src/adventure/Interaction.java
@@ -21,8 +21,8 @@ public class Interaction {
 		interactions = new ArrayList<>();
 		
 		//add new interactions here
-		interactions.add(new Interaction(Item.hammer, Item.sharp, "rock", true,
-				"You hammer away at the rock, and get some sharprocks."));
+		interactions.add(new Interaction(Item.hammer, Item.sharp_rock, "rock", true,
+				"You hammer away at the rock, and get a sharp rock."));
 		interactions.add(new Interaction(Item.axe, Item.wood, "tree", true,
 				"You chop away at the tree, and get some wood."));
 	}
